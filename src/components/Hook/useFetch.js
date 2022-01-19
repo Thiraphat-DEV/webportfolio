e60dbb81eaca95f4842f353ada88bb5cfdd7b368
Thiraphat <thiraboaty@gmail.com>
2022-01-19 17:44:47 +0700

    change hook

--- src/components/Hook/useFetch.js
+++ src/components/Hook/useFetch.js
@@ -3,14 +3,13 @@ import { useState, useEffect } from "react";
 const useFetch = (url) => {
   const [data, setData] = useState([]);
   const [error, setError] = useState("Error FetchData");
-  const [isLoading, setIsLoading] = useState("Loading...");
 
   const fetchData = async () => {
     try {
       const req = await fetch(url);
       const res = await req.json();
       setData(res);
-      if (isLoading) setIsLoading(isLoading);
+   
     } catch (err) {
       if (err) {
         setError(err);
@@ -21,8 +20,8 @@ const useFetch = (url) => {
 
   useEffect(() => {
     fetchData();
-  }, [url]);
-  return { data, isLoading, error };
+  }, []);
+  return { data, error };
 };
 
 export default useFetch;
